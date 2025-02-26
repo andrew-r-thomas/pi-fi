b835e2a5c79373f9c51eeee3b549376a6fa777a3
Andrew Thomas <andrew@andrewrthomas.com>
2025-02-26 12:46:21 -0800

    checkpoint

--- cmd/inspect_flac/main.go
+++ cmd/inspect_flac/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/mewkiz/flac"
 	"github.com/mewkiz/flac/meta"
@@ -17,6 +18,11 @@ func main() {
 	defer stream.Close()
 
 	log.Printf("unencoded audio md5sum: %032x\n", stream.Info.MD5sum[:])
+	log.Printf("channels: %d\n", stream.Info.NChannels)
+	samples := stream.Info.NSamples
+	sampleRate := stream.Info.SampleRate
+	dur := time.Second * time.Duration(samples/uint64(sampleRate))
+	log.Printf("duration: %v\n", dur)
 	for i, block := range stream.Blocks {
 		log.Printf("block %d: %v\n", i, block.Type)
 		switch block.Type {
@@ -25,6 +31,12 @@ func main() {
 			log.Printf("vendor: %s\n", vc.Vendor)
 			for _, tag := range vc.Tags {
 				log.Printf("tag: {%s: %s}\n", tag[0], tag[1])
+			}
+		case meta.TypeSeekTable:
+			st := block.Body.(*meta.SeekTable)
+			for _, point := range st.Points {
+				seekTime := time.Second * time.Duration(point.SampleNum/uint64(sampleRate))
+				log.Printf("seek point: %v\n", seekTime)
 			}
 		}
 	}
